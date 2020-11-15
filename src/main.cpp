0d86620a666a19e3d5e8dcddb896f1e2bbc08299
Julio <j.d.ferraz2@gmail.com>
2020-11-15 00:20:51 -0300

    Build ready in VSCode on Windows
    (cherry picked from commit b66eb3b7afcc0070a6a818f640e94e9e63dc9b42)

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,8 @@
 #include <tensorflow/c/c_api.h>
 //#include "tensorflow/cc/cc_api.h"
 
-int main() {
-  
-  #if DEBUG_MODE
-    // Test call to Tensorflow API
-    std::cout << "This project uses TensorFlow C library version " << TF_Version() << std::endl;
-  #endif
-
-  constexpr std::size_t kFramesPerSecond{480}; //{20}; //{480}; //{60};
+int main(int argc, char **argv) {
+  constexpr std::size_t kFramesPerSecond{60};
   constexpr std::size_t kMsPerFrame{1000 / kFramesPerSecond};
   constexpr std::size_t kScreenWidth{620};
   constexpr std::size_t kScreenHeight{620};
