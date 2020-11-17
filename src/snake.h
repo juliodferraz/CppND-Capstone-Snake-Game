ee525843157670f55d22514a27d44a588e6ea76a
Julio <j.d.ferraz2@gmail.com>
2020-11-16 21:28:46 -0300

    Underlying world view addition to Snake class

--- src/snake.h
+++ src/snake.h
@@ -17,12 +17,20 @@ class Snake {
       : grid_width(grid_width),
         grid_height(grid_height),
         head_x(grid_width / 2),
-        head_y(grid_height / 2) {}
+        head_y(grid_height / 2),
+        head{static_cast<int>(head_x), static_cast<int>(head_y)},
+        vision{{(grid_width - 1) / 2, grid_height - 1}, {0,0}, std::vector<SDL_Point>{}} {}
 
   void Update();
 
   void GrowBody();
   bool SnakeCell(int x, int y);
+
+  /**
+   *  \brief Updates the current food and body sections location in the snake's vision.
+   *  \param food Current food location in the grid, from the player's perspective.
+   */
+  void SeeWorld(const SDL_Point& food);
 
   /**
    *  \brief Makes the snake alive again, with its initial size, effectively resetting the game. If the snake is already alive, nothing is done.
@@ -47,11 +55,24 @@ class Snake {
   bool alive{true};
   float head_x;
   float head_y;
+
+  /**
+   *  \brief The current discrete location of the snake's head on the grid.
+   */
+  SDL_Point head;
+
   std::vector<SDL_Point> body;
 
  private:
   void UpdateHead();
-  void UpdateBody(SDL_Point &current_cell, SDL_Point &prev_cell);
+  void UpdateBody(const SDL_Point &prev_head_cell);
+
+  /**
+   *  \brief Returns the position of a given world point from the snake's perspective, relative to its head.
+   *  \param point A position in the game screen, from the player's viewpoint.
+   *  \return The equivalent point position from the snake's perspective.
+   */
+  SDL_Point ToSnakeVision(const SDL_Point& point);
 
   bool growing{false};
   int grid_width;
@@ -61,6 +82,31 @@ class Snake {
    *  \brief True, if the snake is autonomous. False, if it's controllable by the player.
    */
   bool automode{true};
+
+  /**
+   *  \brief Struct type representing a snake's perception of the world around it.
+   */
+  struct Vision {
+    /**
+     *  \brief Location of the snake's head used as base reference for its world view. From the snake's perspective, its head is on the middle-bottom of the vision grid, allowing it to see what lies ahead and by its sides.
+     */
+    const SDL_Point head;
+
+    /**
+     *  \brief Location of the food relative to the snake's head and current direction.
+     */
+    SDL_Point food;
+
+    /**
+     *  \brief Location of the body sections relative to the snake's head and current direction.
+     */
+    std::vector<SDL_Point> body;
+  };
+
+  /**
+   *  \brief The snake's perception of the world around it.
+   */
+  struct Vision vision;
 };
 
 #endif
